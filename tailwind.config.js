5f398546b3c7d680ef7134a518d093eb0ceca4b6
Diego Lanesan <diegolanesan@gmail.com>
2022-02-12 05:07:39 -0300

    Next.js and Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./ui/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
