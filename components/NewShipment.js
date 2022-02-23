3bb19749f3acb601686125d90f13cf6fb1607a7e
Diego Lanesan <diegolanesan@gmail.com>
2022-02-23 10:49:08 -0300

    Loading state and input validation improved

--- components/NewShipment.js
+++ components/NewShipment.js
@@ -1,92 +1,103 @@
-import { Formik, Form } from 'formik'
-import { useRouter } from 'next/router'
-import { useDispatch, useSelector } from 'react-redux'
-import { createShipment } from '../redux/actions'
-import Input from '@/ui/Input'
-import Button from '@/ui/Button'
-import Title from '@/ui/Title'
+import {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {useRouter} from 'next/router';
+import {Formik, Form} from 'formik';
+import {createShipment} from '../redux/actions';
+import Input from '../ui/Input';
+import Button from '../ui/Button';
+import Title from '../ui/Title';
+import Spinner from '../ui/Spinner';
 
 const NewShipment = () => {
-  const dispatch = useDispatch()
-  const ruouter = useRouter()
-  const shipmentData = useSelector((state) => state.shipments.shipmentData)
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const router = useRouter();
 
   return (
-    <div className="flex justify-center text-center mt-8">
+    <div className="flex justify-center mt-8 text-center">
       <Formik
         initialValues={{
           zip_origin: '',
           zip_destination: '',
           heigth: '',
           width: '',
-          length: ''
+          length: '',
         }}
         validate={(values) => {
-          const errors = {}
-          if (!values.zip_origin) errors.zip_origin = 'Requerido'
-          if (!values.zip_destination) errors.zip_destination = 'Requerido'
-          if (!values.heigth) errors.heigth = 'Requerido'
-          if (!values.width) errors.width = 'Requerido'
-          if (!values.length) errors.length = 'Requerido'
-          return errors
+          const errors = {};
+          if (!values.zip_origin) errors.zip_origin = 'Requerido';
+          if (!values.zip_destination) errors.zip_destination = 'Requerido';
+          if (!values.heigth) {
+            errors.heigth = 'Requerido';
+          } else if ( !/^\d+$/.test(values.heigth) ) {
+            errors.heigth = 'Formato inválido';
+          }
+          if (!values.width) {
+            errors.width = 'Requerido';
+          } else if ( !/^\d+$/.test(values.width) ) {
+            errors.width = 'Formato inválido';
+          }
+          if (!values.length) {
+            errors.length = 'Requerido';
+          } else if ( !/^\d+$/.test(values.length) ) {
+            errors.length = 'Formato inválido';
+          }
+          return errors;
         }}
-        onSubmit={(values, { setSubmitting }) => {
+        onSubmit={(values, {setSubmitting}) => {
+          setLoading(true);
           dispatch(createShipment(values)).then(() => {
-            ruouter.push('/elegir-servicio')
-          })
-          setSubmitting(false)
+            setLoading(false);
+            router.push('/elegir-servicio');
+          });
+          setSubmitting(false);
         }}
       >
-        {({ handleChange, isSubmitting }) => (
+        {({isSubmitting}) => (
           <Form>
-            <div className="flex flex-col gap-8 items-center w-4/5 mx-auto">
-              <div className="flex flex-col sm:flex-row gap-4">
+            <div className="flex flex-col gap-8 items-center mx-auto w-4/5">
+              <div className="flex flex-col gap-4 sm:flex-row">
                 <div>
                   <Title> Origen </Title>
-                  <Input
-                    placeholder="Código postal"
-                    name="zip_origin"
-                    defaultValue={shipmentData ? shipmentData.zip_origin : ''}
-                  />
+                  <Input placeholder="Código postal" name="zip_origin"/>
                 </div>
                 <div>
                   <Title> Destino </Title>
-                  <Input
-                    placeholder="Código postal"
-                    name="zip_destination"
-                    defaultValue={shipmentData ? shipmentData.zip_destination : ''}
-                  />
+                  <Input placeholder="Código postal" name="zip_destination"/>
                 </div>
               </div>
-
               <div>
                 <Title> Medidas del paquete </Title>
-                <div className="flex flex-wrap gap-4 mt-4 justify-center">
+                <div className="flex flex-wrap gap-4 justify-center mt-4">
                   {[
-                    { name: 'heigth', text: 'Alto (cm.)' },
-                    { name: 'width', text: 'Ancho (cm.)' },
-                    { name: 'length', text: 'Largo (cm.)' }
+                    {name: 'heigth', text: 'Alto (cm.)'},
+                    {name: 'width', text: 'Ancho (cm.)'},
+                    {name: 'length', text: 'Largo (cm.)'},
                   ].map((item, index) => (
                     <Input
                       placeholder={item.text}
                       name={item.name}
                       key={index}
-                      defaultValue={shipmentData ? shipmentData[`${item.name}`] : ''}
                     />
                   ))}
                 </div>
               </div>
               <div className="w-[199px] sm:w-[414px]">
-                <Button type="submit" disabled={isSubmitting}>
+                {loading ? <Button disabled type="button">
+                  <Spinner />
+                  Loading...
+                </Button> :
+                 <Button type="submit" disabled={isSubmitting}>
                   Enviar
-                </Button>
+                 </Button>}
+
               </div>
             </div>
           </Form>
         )}
       </Formik>
     </div>
-  )
-}
+  );
+};
 
-export default NewShipment
+export default NewShipment;
